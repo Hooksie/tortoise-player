92f6b2e5c97dad42e58de39fc6b2c05c42a78c2a
Matt Hooks <me@matthooks.com>
2013-04-21 19:40:58 -0400

    Cleared sdcard_init

---
+++ sdcard_init.h
@@ -0,0 +1,13 @@
+/*
+ * sdcard_init.h
+ *
+ *  Created on: Apr 21, 2013
+ *      Author: k120
+ */
+
+#ifndef SDCARD_INIT_H_
+#define SDCARD_INIT_H_
+
+void sdcard_init(void);
+
+#endif /* SDCARD_INIT_H_ */
